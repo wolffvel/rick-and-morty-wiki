f8c58f66140a28c64dc562a399b347380ae01b6b
Pablo Wolff <pabloawolff@gmail.com>
2019-09-06 10:31:10 -0500

    added graphql

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { Router } from '@reach/router';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 
+const client = new ApolloClient({
+  uri: 'https://rickandmortyapi.com/graphql/'
+});
+
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Header />
       <Router className='body'>
         <Home path='/' />
         <About path='/about' />
       </Router>
       <Footer />
-    </>
+    </ApolloProvider>
   );
 }
 
