190d2866e386cc37dabba01c130f4975391ceaf6
Pablo Wolff <pabloawolff@gmail.com>
2019-09-16 15:36:23 -0500

    corrected cards styling

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -4,23 +4,25 @@ const CharacterCard = props => {
   const { name, imgUrl, status, species, origin, lastLocation } = props;
   return (
     <div className='card'>
-      <img src={imgUrl} alt='Character' />
-      <h3>{name}</h3>
+      <img src={imgUrl} alt='Character' className='card-img' />
       <div className='card-container'>
-        <span>Status</span>
-        <p>{status}</p>
+        <h3 className='card-text'>{name}</h3>
       </div>
       <div className='card-container'>
-        <span>Species</span>
-        <p>{species}</p>
+        <span className='card-span'>Status:</span>
+        <p className='card-text'>{status}</p>
       </div>
       <div className='card-container'>
-        <span>Origin</span>
-        <p>{origin}</p>
+        <span className='card-span'>Species:</span>
+        <p className='card-text'>{species}</p>
       </div>
       <div className='card-container'>
-        <span>Location</span>
-        <p>{lastLocation}</p>
+        <span className='card-span'>Origin:</span>
+        <p className='card-text'>{origin}</p>
+      </div>
+      <div className='card-container'>
+        <span className='card-span'>Location:</span>
+        <p className='card-text'>{lastLocation}</p>
       </div>
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,12 @@ const Footer = () => {
   const date = new Date();
   return (
     <footer className='footer'>
-      <p className='footer-text'>{date.getFullYear()}</p>
+      <p className='footer-text'>
+        {date.getFullYear()} - Created by{' '}
+        <a className='footer-text' href='https://github.com/wolffvel'>
+          Pablo Wolff
+        </a>
+      </p>
     </footer>
   );
 };
